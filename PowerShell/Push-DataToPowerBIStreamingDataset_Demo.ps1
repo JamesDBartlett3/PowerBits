f0cc2342883bdb8438b9df7954d278d254f7cdb4
James D. Bartlett III <james.bartlett@gmail.com>
2022-10-09 18:51:40 -0500

    converted comment block

--- PowerShell/Push-DataToPowerBIStreamingDataset_Demo.ps1
+++ PowerShell/Push-DataToPowerBIStreamingDataset_Demo.ps1
@@ -1,11 +1,21 @@
 <# 
+  .SYNOPSIS
+    Function: Push-DataToPowerBIStreamingDataset_Demo
+    Author: @JamesDBartlett3 (James D. Bartlett III)
 
-Title: Push-DataToPowerBIStreamingDataset_Demo.ps1
-Description: Power BI Push Datasets Demo
-Author: @jamesdbartlett3
+  .DESCRIPTION
+    - Power BI Push Datasets Demo
 
-Setup instructions: 
-http://blogs.lobsterpot.com.au/2020/07/16/getting-started-with-power-bi-push-datasets-via-rest-apis
+  .PARAMETERS
+  - 
+
+  .SETUP
+    - http://blogs.lobsterpot.com.au/2020/07/16/getting-started-with-power-bi-push-datasets-via-rest-apis
+
+  .TODO
+    - Convert to function
+    - Parameterize (endpoint, categories, min/max random value, etc.)
+    - Determine if token is needed and implement if so
 
 #>
 
@@ -13,24 +23,24 @@ $endpoint = "" # Endpoint URL
 
 while($true) {
 
-$timeStamp = (Get-Date).ToUniversalTime().ToString('yyyy-MM-ddTHH:mmZ')
-$randomNumber0 = (Get-Random -Maximum 100) / 100
-$randomNumber1 = Get-Random -Maximum 500
-$randomString = "yes", "no", "maybe", "meh" | Get-Random
-$randomColor = "red", "yellow", "blue" | Get-Random
+  $timeStamp = (Get-Date).ToUniversalTime().ToString('yyyy-MM-ddTHH:mmZ')
+  $randomNumber0 = (Get-Random -Maximum 100) / 100
+  $randomNumber1 = Get-Random -Maximum 500
+  $randomString = "yes", "no", "maybe", "meh" | Get-Random
+  $randomColor = "red", "yellow", "blue" | Get-Random
 
-$payload = $null
+  $payload = $null
 
-$payload = @{
-  "someNumber0" = $randomNumber0
-  "someNumber1" = $randomNumber1
-  "someText" = $randomString
-  "someColor" = $randomColor
-  "timeStamp" = $timeStamp
-}
+  $payload = @{
+    "someNumber0" = $randomNumber0
+    "someNumber1" = $randomNumber1
+    "someText" = $randomString
+    "someColor" = $randomColor
+    "timeStamp" = $timeStamp
+  }
 
-Invoke-RestMethod -Method Post -Uri "$endpoint" -Body (ConvertTo-Json @($payload)) | Out-Null
+  Invoke-RestMethod -Method Post -Uri "$endpoint" -Body (ConvertTo-Json @($payload)) | Out-Null
 
-Start-Sleep -Seconds 7.5 | Out-Null
+  Start-Sleep -Seconds 7.5 | Out-Null
 
 }
