0e6b38b372c9886b8daf1e762f75816353ad08e6
James D. Bartlett III <james.bartlett@gmail.com>
2022-10-11 20:06:17 -0500

    updated import script

--- PowerShell/PSHacks4PowerBI/Import-PSHacks4PowerBIFunctions.ps1
+++ PowerShell/PSHacks4PowerBI/Import-PSHacks4PowerBIFunctions.ps1
@@ -34,8 +34,8 @@ $functions = @(
 
 # Dotsource all functions in current directory whose names match those in $functions array
 Get-ChildItem -Path $PSScriptRoot -Filter *.ps1 -Recurse |
-    Where-Object -Property Name -In $functions |
+    Where-Object {$_.Name -in $functions -and $_.FullName -ne $PSCommandPath} |
     ForEach-Object {
         Write-Output "Importing `e[38;2;0;255;0m$($_.BaseName)`e[0m..."
-        . $($_.FullName)
+        . $_.FullName
     }
