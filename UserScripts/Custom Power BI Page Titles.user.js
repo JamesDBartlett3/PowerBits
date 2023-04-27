bc4e0656c664d473e3182ad17f19913da7eca597
James Bartlett <jbartlett@dmu.edu>
2023-04-27 14:10:15 -0500

    added Custom Power BI Page Titles userscript

---
+++ UserScripts/Custom Power BI Page Titles.user.js
@@ -0,0 +1,87 @@
+// ==UserScript==
+// @name         Custom Power BI Page Titles
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Specify custom titles for specific pages in Power BI, so the browser tab will actually say what's on that page instead of just "Power BI."
+// @author       @JamesDBartlett3 (techhub.social/@JamesDBartlett3, github.com/JamesDBartlett3)
+// @match        https://app.powerbi.com/groups/*/list
+// @match        https://app.powerbi.com/groups/*/settings
+// @icon         https://app.powerbi.com/images/PowerBI_Favicon.ico
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	'use strict';
+
+	function getKeyByValue(object, value) {
+        return Object.keys(object)
+			.find(key => value.includes(object[key]));
+	}
+	let tabNames = {
+		"Exec [Pipe]": "ac8ae510-54f4-4046-a64c-58c7b2541b89"
+		, "Exec [Dev]": "ab86bc72-796d-41dc-9201-15954a866758"
+		, "Exec [Test]": "82f58e8b-cfd3-42f1-b723-ce199188d2fc"
+		, "Exec [Prod]": "0691620f-dab9-480b-816c-31c72880b903"
+		, "Admi [Pipe]": "073c8496-1820-437d-8071-ae277ecd8aa3"
+		, "Admi [Dev]": "b43f46d5-5c97-4a7a-9c7f-1ad0ef189365"
+		, "Admi [Test]": "8ba5087a-d778-4b10-8b50-36ce65304ef0"
+		, "Admi [Prod]": "d083e445-e431-47d8-970f-9f8723032404"
+		, "_Dataflows": "f48aaaa7-3a82-4897-92b4-ef797db67f5f"
+		, "_Datasets": "288311c7-879a-495c-b485-56b2fb347f50"
+		, "_DMU_Store": "275d531f-04b0-4081-be1e-51537ec18b4f"
+		, "_DMU_D2L": "c5208007-3114-420b-bb97-3e75ee613b86"
+	}
+
+	function setTitle(t) {
+		document.title = t;
+	}
+	for (var key in tabNames) {
+		if (document.URL.includes(tabNames[key])) {
+			setTitle(key);
+		}
+	};
+	(function(history) {
+		var pushState = history.pushState;
+		history.pushState = function(state, key, path) {
+			if (typeof history.onpushstate == "function") {
+				history.onpushstate({
+					state: state
+					, path: path
+				})
+			}
+			pushState.apply(history, arguments)
+		}
+		window.onpopstate = history.onpushstate = function(e) {
+			var pageName = getKeyByValue(tabNames, e.path)
+			if (pageName) {
+				document.title = pageName
+			}
+		}
+	})(window.history);
+})();
+
+
+// JS Fetch from loading the Power BI homepage
+/* fetch("https://wabi-us-north-central-h-primary-redirect.analysis.windows.net/metadata/app?preferReadOnlySession=true&cmd=home", {
+  "headers": {
+    "accept": "application/json",
+    "accept-language": "en-US,en;q=0.9",
+    "activityid": "67a185d8-643d-4cba-a7ca-38b9374b57ab",
+    "authorization": "Bearer XXXXX",
+    "cache-control": "no-cache",
+    "pragma": "no-cache",
+    "requestid": "ed05a623-10db-4d21-8903-620608627636",
+    "sec-ch-ua": "\"Chromium\";v=\"112\", \"Microsoft Edge\";v=\"112\", \"Not:A-Brand\";v=\"99\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "cross-site"
+  },
+  "referrer": "https://app.powerbi.com/",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": null,
+  "method": "GET",
+  "mode": "cors",
+  "credentials": "include"
+}); */
