556ba66735bf4967e8968c24d39df0b67fc3604b
James D. Bartlett III <james.bartlett@gmail.com>
2023-09-06 19:46:48 -0500

    added process block, now works in pipeline

--- PowerShell/Export-PowerBIBareDatasetFromWorkspace.psm1
+++ PowerShell/Export-PowerBIBareDatasetFromWorkspace.psm1
@@ -66,19 +66,19 @@ Function Export-PowerBIBareDatasetFromWorkspace {
 		[Parameter(
 			Mandatory = $true,
 			ValueFromPipelineByPropertyName = $true
-		)][Alias('Id')][guid[]]$DatasetId,
+		)][Alias('Id')][guid]$DatasetId,
 		[Parameter(
 			Mandatory = $true,
 			ValueFromPipelineByPropertyName = $true
-		)][guid[]]$WorkspaceId,
+		)][guid]$WorkspaceId,
 		[Parameter(
 			Mandatory = $false
 			, ValueFromPipelineByPropertyName = $true
-		)][Alias('Name')][string[]]$DatasetName,
+		)][Alias('Name')][string]$DatasetName,
 		[Parameter(
 			Mandatory = $false
 			, ValueFromPipelineByPropertyName = $true
-		)][string[]]$WorkspaceName,
+		)][string]$WorkspaceName,
 		[Parameter(
 			Mandatory = $false
 			, ValueFromPipeline = $false
@@ -89,140 +89,146 @@ Function Export-PowerBIBareDatasetFromWorkspace {
 		)][string]$OutFile
 	)
 
-	Write-Debug "DatasetId: $DatasetId, WorkspaceId: $WorkspaceId, DatasetName: $DatasetName, WorkspaceName: $WorkspaceName"
+	process {
 
-	#Requires -PSEdition Core -Modules MicrosoftPowerBIMgmt
-	
-	$headers = New-Object 'System.Collections.Generic.Dictionary[[String],[String]]'
-	
-	[string]$tempFolder = Join-Path -Path $env:TEMP -ChildPath 'PowerBIBareDatasets'
-	[string]$blankPbixTempFile = Join-Path -Path $env:TEMP -ChildPath 'blank.pbix'
-	[string]$urlRegex = '(http[s]?|[s]?ftp[s]?)(:\/\/)([^\s,]+)'
-	[string]$uniqueName = 'temp_' + [guid]::NewGuid().ToString().Replace('-', '')
-	[string]$pbiApiBaseUri = 'https://api.powerbi.com/v1.0/myorg'
-	[array]$validPbixContents = @('Layout', 'Metadata')
-	[bool]$blankPbixIsUrl = $BlankPbix -Match $urlRegex
-	[bool]$localFileExists = Test-Path $BlankPbix
-	[bool]$remoteFileIsValid = $false
-	[bool]$localFileIsValid = $false
-	[bool]$defaultFileIsValid = $false
-	
-	Function FileIsBlankPbix($file) {
-		$zip = [System.IO.Compression.ZipFile]::OpenRead($file)
-		$fileIsPbix = @($validPbixContents | Where-Object { $zip.Entries.Name -Contains $_ }).Count -gt 0
-		$fileIsBlank = (Get-Item $file).length / 1KB -lt 20
-		$zip.Dispose()
-		if ($fileIsPbix -and $fileIsBlank) {
-			Write-Debug "$file is a valid blank PBIX file."
-			return $true
+		Write-Debug "DatasetId: $DatasetId, WorkspaceId: $WorkspaceId, DatasetName: $DatasetName, WorkspaceName: $WorkspaceName"
+
+		#Requires -PSEdition Core -Modules MicrosoftPowerBIMgmt
+		
+		$headers = New-Object 'System.Collections.Generic.Dictionary[[String],[String]]'
+		
+		[string]$tempFolder = Join-Path -Path $env:TEMP -ChildPath 'PowerBIBareDatasets'
+		[string]$blankPbixTempFile = Join-Path -Path $env:TEMP -ChildPath 'blank.pbix'
+		[string]$urlRegex = '(http[s]?|[s]?ftp[s]?)(:\/\/)([^\s,]+)'
+		[string]$uniqueName = 'temp_' + [guid]::NewGuid().ToString().Replace('-', '')
+		[string]$pbiApiBaseUri = 'https://api.powerbi.com/v1.0/myorg'
+		[array]$validPbixContents = @('Layout', 'Metadata')
+		[bool]$blankPbixIsUrl = $BlankPbix -Match $urlRegex
+		[bool]$localFileExists = Test-Path $BlankPbix
+		[bool]$remoteFileIsValid = $false
+		[bool]$localFileIsValid = $false
+		[bool]$defaultFileIsValid = $false
+		
+		Function FileIsBlankPbix($file) {
+			$zip = [System.IO.Compression.ZipFile]::OpenRead($file)
+			$fileIsPbix = @($validPbixContents | Where-Object { $zip.Entries.Name -Contains $_ }).Count -gt 0
+			$fileIsBlank = (Get-Item $file).length / 1KB -lt 20
+			$zip.Dispose()
+			if ($fileIsPbix -and $fileIsBlank) {
+				Write-Debug "$file is a valid blank PBIX file."
+				return $true
+			}
+			else {
+				Write-Error "$file is NOT a valid PBIX file and/or NOT blank."
+				return $false
+			}
 		}
+
+		# If the temp folder doesn't exist and user has not specified OutFile location, create the temp folder
+		if (!(Test-Path -LiteralPath $tempFolder) -and !$OutFile) {
+			New-Item -Path $tempFolder -ItemType Directory | Out-Null
+		}
+		
+		# If user specified a URL to a file, download and validate it as a blank PBIX file
+		if ($blankPbixIsUrl) {
+			Write-Debug "Downloading file: $BlankPbix..."
+			Invoke-WebRequest -Uri $BlankPbix -OutFile $blankPbixTempFile
+			Write-Debug 'Validating downloaded file...'
+			$remoteFileIsValid = FileIsBlankPbix($blankPbixTempFile)
+		}
+		
+		# If user specified a local path to a file, validate it as a blank PBIX file
+		elseif ($localFileExists) {
+			Write-Debug "Validating user-supplied file: $BlankPbix..."
+			$localFileIsValid = FileIsBlankPbix($BlankPbix)
+		}
+		
+		# If user didn't specify a blank PBIX file, check for a valid blank PBIX in the temp location
+		elseif (Test-Path $blankPbixTempFile) {
+			Write-Debug "Validating pbix file found in temp location: $blankPbixTempFile..."
+			$defaultFileIsValid = FileIsBlankPbix($blankPbixTempFile)
+		}
+		
+		# If user did not specify a blank PBIX file, and a valid blank PBIX is not in the temp location,
+		# download one from GitHub, and then check if it's valid and blank
 		else {
-			Write-Error "$file is NOT a valid PBIX file and/or NOT blank."
-			return $false
+			Write-Debug "Downloading a blank pbix file from GitHub to $blankPbixTempFile..."
+			$BlankPbixUri = 'https://github.com/JamesDBartlett3/PowerBits/raw/main/Misc/blank.pbix'
+			Invoke-WebRequest -Uri $BlankPbixUri -OutFile $blankPbixTempFile
+			$defaultFileIsValid = FileIsBlankPbix($blankPbixTempFile)
 		}
-	}
-
-	# If the temp folder doesn't exist and user has not specified OutFile location, create the temp folder
-	if (!(Test-Path -LiteralPath $tempFolder) -and !$OutFile) {
-		New-Item -Path $tempFolder -ItemType Directory | Out-Null
-	}
-	
-	# If user specified a URL to a file, download and validate it as a blank PBIX file
-	if ($blankPbixIsUrl) {
-		Write-Debug "Downloading file: $BlankPbix..."
-		Invoke-WebRequest -Uri $BlankPbix -OutFile $blankPbixTempFile
-		Write-Debug 'Validating downloaded file...'
-		$remoteFileIsValid = FileIsBlankPbix($blankPbixTempFile)
-	}
-	
-	# If user specified a local path to a file, validate it as a blank PBIX file
-	elseif ($localFileExists) {
-		Write-Debug "Validating user-supplied file: $BlankPbix..."
-		$localFileIsValid = FileIsBlankPbix($BlankPbix)
-	}
-	
-	# If user didn't specify a blank PBIX file, check for a valid blank PBIX in the temp location
-	elseif (Test-Path $blankPbixTempFile) {
-		Write-Debug "Validating pbix file found in temp location: $blankPbixTempFile..."
-		$defaultFileIsValid = FileIsBlankPbix($blankPbixTempFile)
-	}
-	
-	# If user did not specify a blank PBIX file, and a valid blank PBIX is not in the temp location,
-	# download one from GitHub, and then check if it's valid and blank
-	else {
-		Write-Debug "Downloading a blank pbix file from GitHub to $blankPbixTempFile..."
-		$BlankPbixUri = 'https://github.com/JamesDBartlett3/PowerBits/raw/main/Misc/blank.pbix'
-		Invoke-WebRequest -Uri $BlankPbixUri -OutFile $blankPbixTempFile
-		$defaultFileIsValid = FileIsBlankPbix($blankPbixTempFile)
-	}
-	
-	# If we downloaded a valid blank PBIX file, use it.
-	if ($remoteFileIsValid -or $defaultFileIsValid) {
-		$BlankPbix = $blankPbixTempFile
-	}
-	
-	# If a valid blank PBIX file could not be obtained by any of the above methods, throw an error.
-	if (!$localFileIsValid -and !$remoteFileIsValid -and !$defaultFileIsValid) {
-		Write-Error 'No blank PBIX file found. Please specify a valid blank PBIX file using the -BlankPbix parameter.'
-		return
-	}
-	
-	try {
-		$headers = Get-PowerBIAccessToken
-	} 
-	catch {
-		Write-Host '🔒 Power BI Access Token required. Launching Azure Active Directory authentication dialog...'
-		Start-Sleep -s 1
-		Connect-PowerBIServiceAccount -WarningAction SilentlyContinue | Out-Null
-		$headers = Get-PowerBIAccessToken
-		Write-Host '🔑 Power BI Access Token acquired. Proceeding...'
-
-	} 
-	finally {
-
-		# Publish the blank PBIX file to the target workspace
-		Write-Debug "Publishing $BlankPbix to workspace with temporary name $uniqueName"
-		$publishResponse = New-PowerBIReport -Path $BlankPbix -WorkspaceId $WorkspaceId -Name $uniqueName -ConflictAction CreateOrOverwrite
-		Write-Debug "Response: $publishResponse"
-		$publishedReportId = $publishResponse.Id
-		$publishedDatasetId = (Get-PowerBIDataset -WorkspaceId $WorkspaceId | Where-Object { $_.Name -eq $uniqueName }).Id
-		Write-Debug "Published report ID: $publishedReportId; Published dataset ID: $publishedDatasetId"
-
-		# Assemble the Datasets API URI
-		$datasetsEndpoint = "$pbiApiBaseUri/groups/$WorkspaceId/datasets"
-		# Assemble the Reports API URI
-		$reportsEndpoint = "$pbiApiBaseUri/groups/$WorkspaceId/reports"
-		# Assemble the Rebind API URI and request body
-		$updateReportContentEndpoint = "$pbiApiBaseUri/groups/$WorkspaceId/reports/$publishedReportId/Rebind"
-		# Assemble the request body
-		$body = "{`"datasetId`": `"$DatasetId`"}"
-
-		# Add the Content-Type header to the request
-		$headers.Add('Content-Type', 'application/json')
-
-		# Rebind the published Report to the bare Dataset
-		Write-Debug "Rebinding published report $publishedReportId to dataset $DatasetId..."
-		Invoke-RestMethod -Uri $updateReportContentEndpoint -Method POST -Headers $headers -Body $body
-
-		# If user did not specify an output file name, use the Dataset's name and save it in the default temp folder
-		$OutFile = if (!!$OutFile) { $OutFile } else {
-			$datasetName = (Get-PowerBIDataset -Id $DatasetId -WorkspaceId $WorkspaceId).Name
-			Join-Path -Path $tempFolder -ChildPath "$($datasetName).pbix"
-			Invoke-Item -Path $tempFolder
+		
+		# If we downloaded a valid blank PBIX file, use it.
+		if ($remoteFileIsValid -or $defaultFileIsValid) {
+			$BlankPbix = $blankPbixTempFile
 		}
+		
+		# If a valid blank PBIX file could not be obtained by any of the above methods, throw an error.
+		if (!$localFileIsValid -and !$remoteFileIsValid -and !$defaultFileIsValid) {
+			Write-Error 'No blank PBIX file found. Please specify a valid blank PBIX file using the -BlankPbix parameter.'
+			return
+		}
+		
+		try {
+			$headers = Get-PowerBIAccessToken
+		}
+		catch {
+			Write-Host '🔒 Power BI Access Token required. Launching Azure Active Directory authentication dialog...'
+			Start-Sleep -s 1
+			Connect-PowerBIServiceAccount -WarningAction SilentlyContinue | Out-Null
+			$headers = Get-PowerBIAccessToken
+			Write-Host '🔑 Power BI Access Token acquired. Proceeding...'
 
-		# Export the re-bound Report and Dataset (a.k.a. "Thick Report") as a PBIX file
-		Write-Debug "Exporting re-bound blank report and dataset (a.k.a. 'thick report') $publishedReportId to $OutFile..."
+		} 
+		finally {
 
-		# Save the PBIX file to a temp file first, then rename it to the correct name (workaround for Datasets with special characters in their names)
-		Export-PowerBIReport -WorkspaceId $WorkspaceId -Id $publishedReportId -OutFile (Join-Path -Path $tempFolder -ChildPath "$uniqueName.pbix")
-		Rename-Item -Path (Join-Path -Path $tempFolder -ChildPath "$uniqueName.pbix") -NewName $OutFile
+			# Publish the blank PBIX file to the target workspace
+			Write-Debug "Publishing $BlankPbix to workspace with temporary name $uniqueName"
+			$publishResponse = New-PowerBIReport -Path $BlankPbix -WorkspaceId $WorkspaceId -Name $uniqueName -ConflictAction CreateOrOverwrite
+			Write-Debug "Response: $publishResponse"
+			$publishedReportId = $publishResponse.Id
+			$publishedDatasetId = (Get-PowerBIDataset -WorkspaceId $WorkspaceId | Where-Object { $_.Name -eq $uniqueName }).Id
+			Write-Debug "Published report ID: $publishedReportId; Published dataset ID: $publishedDatasetId"
 
-		# Delete the blank Report and its original Dataset from the workspace
-		Write-Debug "Deleting temporary blank dataset $publishedDatasetId and report $publishedReportId from workspace $WorkspaceId..."
-		Invoke-RestMethod "$datasetsEndpoint/$publishedDatasetId" -Method DELETE -Headers $headers
-		Invoke-RestMethod "$reportsEndpoint/$publishedReportId" -Method DELETE -Headers $headers
+			# Assemble the Datasets API URI
+			$datasetsEndpoint = "$pbiApiBaseUri/groups/$WorkspaceId/datasets"
+			# Assemble the Reports API URI
+			$reportsEndpoint = "$pbiApiBaseUri/groups/$WorkspaceId/reports"
+			# Assemble the Rebind API URI and request body
+			$updateReportContentEndpoint = "$pbiApiBaseUri/groups/$WorkspaceId/reports/$publishedReportId/Rebind"
+			# Assemble the request body
+			$body = "{`"datasetId`": `"$DatasetId`"}"
+
+			# Add the Content-Type header to the request
+			$headers.Add('Content-Type', 'application/json')
+
+			# Rebind the published Report to the bare Dataset
+			Write-Debug "Rebinding published report $publishedReportId to dataset $DatasetId..."
+			Invoke-RestMethod -Uri $updateReportContentEndpoint -Method POST -Headers $headers -Body $body
+
+			# If user did not specify an output file name, use the Dataset's name and save it in the default temp folder
+			$OutFile = if (!!$OutFile) { $OutFile } else {
+				$datasetName = (Get-PowerBIDataset -Id $DatasetId -WorkspaceId $WorkspaceId).Name
+				Join-Path -Path $tempFolder -ChildPath "$($datasetName).pbix"
+				Invoke-Item -Path $tempFolder
+			}
+
+			# Export the re-bound Report and Dataset (a.k.a. "Thick Report") as a PBIX file
+			Write-Debug "Exporting re-bound blank report and dataset (a.k.a. 'thick report') $publishedReportId to $OutFile..."
+
+			# Save the PBIX file to a temp file first, then rename it to the correct name (workaround for Datasets with special characters in their names)
+			$tempFileName = Join-Path -Path $tempFolder -ChildPath "$uniqueName.pbix"
+			Export-PowerBIReport -WorkspaceId $WorkspaceId -Id $publishedReportId -OutFile $tempFileName
+			Rename-Item -Path $tempFileName -NewName $OutFile
+			$OutFile = $null
+
+			# Delete the blank Report and its original Dataset from the workspace
+			Write-Debug "Deleting temporary blank dataset $publishedDatasetId and report $publishedReportId from workspace $WorkspaceId..."
+			Invoke-RestMethod "$datasetsEndpoint/$publishedDatasetId" -Method DELETE -Headers $headers
+			Invoke-RestMethod "$reportsEndpoint/$publishedReportId" -Method DELETE -Headers $headers
+			
+		}
 		
 	}
-	
+
 }
