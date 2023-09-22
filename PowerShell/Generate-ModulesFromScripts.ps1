2218e75a172fbc4b37272c0d306d68d17cbe354d
James Bartlett <jbartlett@dmu.edu>
2023-09-22 16:13:08 -0500

    combines groups of scripts into module files

--- PowerShell/Generate-ModulesFromScripts.ps1
+++ PowerShell/Generate-ModulesFromScripts.ps1
@@ -3,8 +3,10 @@
 foreach ($module in $ModuleList) {
 		$module.name
 		foreach ($function in $module.functions) {
-				$function
+				"Module: $($module.name) Function: $function"
 				$script = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath "Scripts/$($function).ps1")
-				$script | Add-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath "$($function).psm1") -WhatIf
+				"Function $function {`n" | Add-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath "$($module.name)_test.psm1")
+				$script | Add-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath "$($module.name)_test.psm1")
+				"`n}" | Add-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath "$($module.name)_test.psm1")
 		}
 }
