d83b8b13e1432c83e0391e0192f1ad7fdf6edae7
James Bartlett <jbartlett@dmu.edu>
2023-09-22 15:58:15 -0500

    created Get-ArrayInBatches.ps1

---
+++ PowerShell/Scripts/Get-ArrayInBatches.ps1
@@ -0,0 +1,22 @@
+# Credit: @ruiromano on GitHub
+# Source: https://github.com/RuiRomano/pbimonitor
+
+[CmdletBinding()]
+Param(
+	[Parameter(Mandatory)][array]$array,
+	[Parameter(Mandatory)][int]$batchCount,
+	[Parameter(Mandatory)][ScriptBlock]$script,
+	[Parameter][string]$label = "Get-ArrayInBatches"
+)
+$skip = 0
+$i = 0
+do {
+	$batchItems = @($array | Select-Object -First $batchCount -Skip $skip)
+	if ($batchItems) {
+		Write-Host "[$label] Batch: $($skip + $batchCount) / $($array.Count)"
+		Invoke-Command -ScriptBlock $script -ArgumentList @($batchItems, $i)
+		$skip += $batchCount
+	}
+	$i++
+}
+while($batchItems.Count -ne 0 -and $batchItems.Count -ge $batchCount)
