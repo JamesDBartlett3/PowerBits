f0a1d0a169dc6d273e968c8ff3b185fbd1455bcd
James Bartlett <jbartlett@dmu.edu>
2023-09-22 13:34:43 -0500

    add Wait-On429Error.psm1

---
+++ PowerShell/Wait-On429Error.psm1
@@ -0,0 +1,28 @@
+# Credit: @ruiromano on GitHub
+# Source: https://github.com/RuiRomano/pbimonitor
+function Wait-On429Error {
+	[CmdletBinding()]
+	Param(
+		[Parameter(Mandatory)][ScriptBlock]$script,
+		[Parameter][int]$sleepSeconds = 3601,
+		[Parameter][int]$tentatives = $null
+	)
+	try {
+		Invoke-Command -ScriptBlock $script
+	} catch {
+		$ex = $_.Exception
+		$errorText = $ex.ToString()
+		if ($errorText -like '*HttpRequestException*' -and $errorText -like '*429*') {
+			Write-Host "'429 (Too Many Requests)' Error - Sleeping for $sleepSeconds seconds before trying again..." -ForegroundColor Yellow
+			if ($tentatives) {$tentatives = $tentatives - 1}
+			if ($tentatives -eq 0) {
+				throw '[Wait-On429Error] Max tentatives reached!'
+			} else {
+				Start-Sleep -Seconds $sleepSeconds
+				Wait-On429Error -script $script -sleepSeconds $sleepSeconds -tentatives $tentatives
+			}
+		} else {
+			throw
+		}
+	}
+}
