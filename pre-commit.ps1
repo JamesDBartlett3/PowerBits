f61a714656b7e3604ba48ec0c1a9924e30387b5e
James Bartlett <jbartlett@dmu.edu>
2023-09-22 19:04:41 -0500

    added notes to pre-commit script

--- pre-commit.ps1
+++ pre-commit.ps1
@@ -3,3 +3,11 @@ Set-Location $(git rev-parse --show-toplevel)
 . './PowerShell/Generate-ModulesFromScripts.ps1'
 git add "./PowerShell/Modules/*"
 exit $LASTEXITCODE
+
+<#
+Contents of pre-commit file in .git/hooks folder:
+#!/bin/sh
+echo 
+exec pwsh.exe -ExecutionPolicy RemoteSigned -File '.\pre-commit.ps1'
+exit
+#>
