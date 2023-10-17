9cd57e64ef75c02620dd9b87c8962ed1bd285229
James Bartlett <jbartlett@dmu.edu>
2023-10-17 08:21:00 -0500

    added fn_GetTableFromDataflow.m

---
+++ PowerQuery/fn_GetTableFromDataflow.m
@@ -0,0 +1,16 @@
+(WorkspaceNameOrId as text, DataflowNameOrId as text, TableName as text, RowLimit as number) => 
+let
+	Source = PowerPlatform.Dataflows(null),
+	WorkspaceList = Source{[Id="Workspaces"]}[Data],
+	SelectedWorkspace = 
+		try WorkspaceList{[workspaceName=WorkspaceNameOrId]}[Data]
+		otherwise WorkspaceList{[workspaceId=WorkspaceNameOrId]}[Data],
+	SelectedDataflow = 
+		try SelectedWorkspace{[dataflowName=DataflowNameOrId]}[Data]
+		otherwise SelectedWorkspace{[dataflowId=DataflowNameOrId]}[Data],
+	SelectedTable = SelectedDataflow{[entity=TableName,version=""]}[Data],
+	FilterLogic = if RowLimit < 0
+		then SelectedTable
+		else Table.FirstN(SelectedTable, RowLimit)
+in
+	FilterLogic
