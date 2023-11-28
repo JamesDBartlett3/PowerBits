068e149f43af3e70342e48d16bca9370fafe5149
James Bartlett <jbartlett@dmu.edu>
2023-11-28 13:05:23 -0600

    updated docbloc & default outfile param value

--- PowerShell/Modules/PowerBits-TenantAdmin.psm1
+++ PowerShell/Modules/PowerBits-TenantAdmin.psm1
@@ -110,30 +110,50 @@ Function Export-PowerBIScannerApiData {
   <#
   .SYNOPSIS
     Title: Export-PowerBIScannerApiData
-    Author: @JamesDBartlett3@techhub.social (James D. Bartlett III)
+    Author: James D. Bartlett III
   .DESCRIPTION
     This script will get all available data from the Power BI Scanner API and write it to a JSON file.
+  
+  .INPUTS
+    - Parameters are currently the only way to pass input to this script
+    - Pipeline inputs are not yet supported
+  .OUTPUTS
+    - A .json file containing all available data from the Power BI Scanner API
+    - Pipeline outputs are not yet supported
   .PARAMETER OutFile
-    The destination path for the JSON file. Defaults to the Downloads folder.
+    The destination path for the JSON file. Defaults to "~\Downloads\PowerBIScannerApiData_{timestamp}.json"
   .PARAMETER OpenFile
     Specify to open the JSON file in the default application after it's created.
   .EXAMPLE
-    .\Export-PowerBIScannerApiData.ps1 -OutFile "~\Downloads\PowerBIScannerApiData.json"
+    .\Export-PowerBIScannerApiData.ps1 -OutFile "C:\temp\MyPowerBIScannerApiData.json"
+    # Export data to "C:\temp\MyPowerBIScannerApiData.json"
+  .EXAMPLE
+    .\Export-PowerBIScannerApiData.ps1 -OpenFile
+    # Export data to the default location ("~\Downloads\PowerBIScannerApiData_{timestamp}.json")
+    # and open the file in the system's default .json file handler/editor
   .LINK
-    https://github.com/JamesDBartlett3/PowerBits
+    [Source code](https://github.com/JamesDBartlett3/PowerBits)
   .LINK
-    https://techhub.social/@JamesDBartlett3
+    [The author's blog](https://datavolume.xyz)
+    
   .LINK
-    https://datavolume.xyz
+    [Follow the author on LinkedIn](https://www.linkedin.com/in/jamesdbartlett3/)
+  
+  .LINK
+    [Follow the author on Mastodon](https://techhub.social/@JamesDBartlett3)
+  .LINK
+    [Follow the author on BlueSky](https://bsky.app/profile/jamesdbartlett3.bsky.social)
   .NOTES
     - Requires the Power BI Management module: https://docs.microsoft.com/en-us/powershell/power-bi/overview?view=powerbi-ps
     - Requires the Power BI Scanner API to be enabled: https://learn.microsoft.com/en-us/power-bi/enterprise/service-admin-metadata-scanning#enabling-enhanced-metadata-scanning
     - Currently only works with workspaces that have been modified in the last 30 days
     - Tenants with a lot of workspaces may not work properly due to API rate limits
+    TODO:
+      - Add parameters for all available API options
 #> 
   Param(
     [Parameter(Mandatory = $false)]
-    [string]$OutFile = '~\Downloads\PowerBIScannerApiData.json',
+    [string]$OutFile = "$HOME\Downloads\PowerBIScannerApiData_$(Get-Date -UFormat '%Y-%m-%d_%H%M').json",
     [Parameter(Mandatory = $false)]
     [switch]$OpenFile
   )
