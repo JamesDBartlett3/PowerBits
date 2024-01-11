67f15b7cf3e1dd24163f97a95b3c1b4c6c2b41ad
James Bartlett <jbartlett@dmu.edu>
2024-01-11 00:35:40 -0600

    Add batch processing and filter options to Get-PowerBIDatasourcesFromDatasets.ps1

--- PowerShell/Scripts/.Experimental/Get-PowerBIDatasourcesFromDatasets.ps1
+++ PowerShell/Scripts/.Experimental/Get-PowerBIDatasourcesFromDatasets.ps1
@@ -3,7 +3,8 @@ Param (
   [Parameter()][string]$DatasourceType,
   [Parameter()][string]$DatasourceServer,
   [Parameter()][string]$DatasourceDatabase,
-  [Parameter()][int]$MaxResults
+  [Parameter()][int]$BatchStart,
+  [Parameter()][int]$BatchEnd
 )
 
 #Requires -Modules MicrosoftPowerBIMgmt -PSEdition Core
@@ -11,7 +12,7 @@ Param (
 begin {
   $headers = [System.Collections.Generic.Dictionary[[String], [String]]]::New()
   $pbiApiBaseUrl = 'https://api.powerbi.com/v1.0/myorg'
-  $pbiDatasetsAdminEndpoint = '/admin/datasets'
+  $pbiDatasetsAdminEndpoint = 'admin/datasets'
 }
 
 process {
@@ -37,20 +38,18 @@ process {
     }
   }
 
-  $datasets = (Invoke-RestMethod -Uri "$pbiApiBaseUrl$pbiDatasetsAdminEndpoint" -Headers $headers -Method Get).value
+  $datasets = (Invoke-RestMethod -Uri "$pbiApiBaseUrl/$pbiDatasetsAdminEndpoint" -Headers $headers -Method Get).value
 
   $datasets = $datasets | Where-Object { $_.IsRefreshable -eq $true -and $_.contentProviderType -eq 'PbixInImportMode' }
-  
-  $limit = $MaxResults ?? $datasets.Count
 
-  [PSCustomObject]$datasources = foreach ($dataset in $datasets[0..$limit]) {
+  [PSCustomObject]$datasources = foreach ($dataset in $datasets[$BatchStart..$BatchEnd]) {
 
     $datasetId = $dataset.id
     $datasetName = $dataset.name
     $workspaceId = $dataset.workspaceId
 
     # Get the datasources for the current dataset
-    $datasetDatasources = (Invoke-RestMethod -Uri "$pbiApiBaseUrl$pbiDatasetsAdminEndpoint/$($dataset.id)/datasources" -Headers $headers -Method Get).value
+    $datasetDatasources = (Invoke-RestMethod -Uri "$pbiApiBaseUrl/$pbiDatasetsAdminEndpoint/$($dataset.id)/datasources" -Headers $headers -Method Get).value
 
     # Return the dataset and its datasources
     [PSCustomObject]@{
@@ -66,29 +65,36 @@ process {
     $datasetId = $_.DatasetId
     $datasetName = $_.DatasetName
     $workspaceId = $_.WorkspaceId
-    $datasourceType = $_.Datasources.datasourceType
-    $datasourceServer = $_.Datasources.connectionDetails.server
-    $datasourceDatabase = $_.Datasources.connectionDetails.database
-    $datasourceId = $_.Datasources.datasourceId
-    $datasourceGatewayId = $_.Datasources.gatewayId
+    $dsType = $_.Datasources.datasourceType
+    $dsServer = $_.Datasources.connectionDetails.server
+    $dsDatabase = $_.Datasources.connectionDetails.database
+    $dsId = $_.Datasources.datasourceId
+    $dsGatewayId = $_.Datasources.gatewayId
 
     [PSCustomObject]@{
       DatasetId = $datasetId
       DatasetName = $datasetName
       WorkspaceId = $workspaceId
-      DatasourceType = $datasourceType -join ','
-      DatasourceServer = $datasourceServer -join ','
-      DatasourceDatabase = $datasourceDatabase -join ','
-      DatasourceId = $datasourceId -join ','
-      DatasourceGatewayId = $datasourceGatewayId -join ','
+      DatasourceType = $dsType -join ','
+      DatasourceServer = $dsServer -join ','
+      DatasourceDatabase = $dsDatabase -join ','
+      DatasourceId = $dsId -join ','
+      DatasourceGatewayId = $dsGatewayId -join ','
     }
 
   }
+
+  # Filter results using the parameters
+  if ($DatasourceType) {
+    $result = $result | Where-Object { $_.DatasourceType -contains $DatasourceType }
+  }
+  if ($DatasourceServer) {
+    $result = $result | Where-Object { $_.DatasourceServer -contains $DatasourceServer }
+  }
+  if ($DatasourceDatabase) {
+    $result = $result | Where-Object { $_.DatasourceDatabase -contains $DatasourceDatabase }
+  }
   
-  $result | Where-Object {
-    (if (!$DatasourceType) { $true } else { $_.DatasourceType -contains $DatasourceType }) -and `
-    (if (!$DatasourceServer) { $true } else { $_.DatasourceServer -contains $DatasourceServer }) -and `
-    (if (!$DatasourceDatabase) { $true } else { $_.DatasourceDatabase -contains $DatasourceDatabase })
-  } | Format-List
+  return $result
 
 }
