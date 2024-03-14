5943c1b2c5424989f5ce5f94a8fe6d8eb8480fd5
James Bartlett <jbartlett@dmu.edu>
2024-03-14 16:05:48 -0500

    Fix parameter casing in Get-AzAuthToken.ps1

--- PowerShell/Scripts/.Experimental/Get-AzAuthToken.ps1
+++ PowerShell/Scripts/.Experimental/Get-AzAuthToken.ps1
@@ -1,21 +1,21 @@
 param(
   [Parameter()]
-  [string]$tenantId,
+  [string]$TenantId,
   [Parameter()]
-  [string]$appId,
+  [string]$AppId,
   [Parameter()]
-  [string]$appSecret
+  [string]$AppSecret
 )
 process{
   # If Az authentication has not already been established, attempt to authenticate
-  if (-not (Get-AzContext) -and $null -eq $appSecret) {
+  if (-not (Get-AzContext) -and $null -eq $AppSecret) {
     try {
       # Convert the app secret (a string) to a SecureString
-      $secureAppSecret = ConvertTo-SecureString -String $appSecret -AsPlainText -Force
+      $secureAppSecret = ConvertTo-SecureString -String $AppSecret -AsPlainText -Force
       # Create a PS credential object
-      $psCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $appId, $secureAppSecret
+      $psCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $AppId, $secureAppSecret
       # Attempt to connect to Azure with service principal
-      Connect-AzAccount -ServicePrincipal -Tenant $tenantId -Credential $psCred
+      Connect-AzAccount -ServicePrincipal -Tenant $TenantId -Credential $psCred
     }
     catch {
       # If service principal authentication fails, fall back to interactive user authentication
